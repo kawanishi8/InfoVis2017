2f58f496027f43f2f1146e981dc76d258ea28a81
川西　亮平 <179x005x@lib-shizen-a-013.epc.center.kobe-u.ac.jp>
2017-05-29 16:24:02 +0900

    Initial commit

---
+++ W12/main.js
@@ -0,0 +1,86 @@
+function main1()
+{
+    var volume = new KVS.LobsterData();
+    var screen = new KVS.THREEScreen();
+
+    screen.init( volume, {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        enableAutoResize: false
+    });
+
+    var bounds = Bounds( volume );
+    screen.scene.add( bounds );
+
+    var isovalue = 128;
+    var surfaces = Isosurfaces1( volume, isovalue );
+    screen.scene.add( surfaces );
+
+    document.addEventListener( 'mousemove', function() {
+        screen.light.position.copy( screen.camera.position );
+    });
+
+    window.addEventListener( 'resize', function() {
+        screen.resize( [ window.innerWidth, window.innerHeight ] );
+    });
+
+    screen.loop();
+}
+
+function main2()
+{
+    var volume = new KVS.LobsterData();
+    var screen = new KVS.THREEScreen();
+
+    screen.init( volume, {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        enableAutoResize: false
+    });
+
+    var bounds = Bounds( volume );
+    screen.scene.add( bounds );
+
+    var isovalue = 128;
+    var surfaces = Isosurfaces2( volume, isovalue );
+    screen.scene.add( surfaces );
+
+    document.addEventListener( 'mousemove', function() {
+        screen.light.position.copy( screen.camera.position );
+    });
+
+    window.addEventListener( 'resize', function() {
+        screen.resize( [ window.innerWidth, window.innerHeight ] );
+    });
+
+    screen.loop();
+}
+
+
+function main3() {
+    var volume = new KVS.LobsterData();
+    var screen = new KVS.THREEScreen();
+
+    screen.init(volume, {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        enableAutoResize: false
+    });
+
+    var bounds = Bounds(volume);
+    screen.scene.add(bounds);
+
+    var isovalue = 128;
+    var surfaces = Isosurfaces3(volume, isovalue);
+    screen.scene.add(surfaces);
+
+    document.addEventListener('mousemove', function () {
+        screen.light.position.copy(screen.camera.position);
+    });
+
+    window.addEventListener('resize', function () {
+        screen.resize([window.innerWidth, window.innerHeight]);
+    });
+
+    screen.loop();
+}
